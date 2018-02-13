a0126f34d24cbd3419ad56320d36421f0695a049
kangaroo0123 <daihanjun@gmail.com>
2018-02-13 16:59:07 -0500

    scp

--- code/s2v_scp/run_eval.sh
+++ code/s2v_scp/run_eval.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
-g_type=clustered
+frac_primal=0.2
+edge_prob=0.05
 
-result_root=results/dqn-$g_type
+nmin_test=15
+nmax_test=20
+
+data_test=../../data/scp/nrange-$nmin_test-$nmax_test-n_graph-1000-p-$edge_prob-frac_primal-$frac_primal-seed-1.pkl
+
+result_root=results
 
 # max belief propagation iteration
-max_bp_iter=4
+max_bp_iter=5
 
 # embedding size
 embed_dim=64
 
 # gpu card id
-dev_id=1
+dev_id=0
 
 # max batch size for training/testing
-batch_size=128
+batch_size=64
 
 net_type=QNet
-decay=0.1
 
 # set reg_hidden=0 to make a linear regression
-reg_hidden=32
+reg_hidden=64
 
 # learning rate
 learning_rate=0.0001
@@ -29,38 +34,28 @@ learning_rate=0.0001
 w_scale=0.01
 
 # nstep
-n_step=1
-
-knn=10
+n_step=2
 
 min_n=15
 max_n=20
 
-num_env=1
-mem_size=50000
+num_env=10
+mem_size=500000
 
-max_iter=200000
+max_iter=100000
 
 # folder to save the trained model
-save_dir=$result_root/ntype-$net_type-embed-$embed_dim-nbp-$max_bp_iter-rh-$reg_hidden
+save_dir=$result_root/embed-$embed_dim-nbp-$max_bp_iter-rh-$reg_hidden-primal-$frac_primal-ep-$edge_prob
 
-if [ ! -e $save_dir ];
-then
-    mkdir -p $save_dir
-fi
-
-python main.py \
-    -net_type $net_type \
+python evaluate.py \
+    -dev_id $dev_id \
     -n_step $n_step \
-    -data_root ../../data/tsp2d \
-    -decay $decay \
-    -knn $knn \
+    -data_test $data_test \
     -min_n $min_n \
     -max_n $max_n \
     -num_env $num_env \
     -max_iter $max_iter \
     -mem_size $mem_size \
-    -g_type $g_type \
     -learning_rate $learning_rate \
     -max_bp_iter $max_bp_iter \
     -net_type $net_type \
@@ -71,5 +66,4 @@ python main.py \
     -reg_hidden $reg_hidden \
     -momentum 0.9 \
     -l2 0.00 \
-    -w_scale $w_scale \
-    2>&1 | tee $save_dir/log-$min_n-${max_n}.txt
+    -w_scale $w_scale
--- code/s2v_scp/run_nstep_dqn.sh
+++ code/s2v_scp/run_nstep_dqn.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 
-g_type=clustered
-
-result_root=results/dqn-$g_type
+result_root=results
 
 # max belief propagation iteration
-max_bp_iter=4
+max_bp_iter=5
 
 # embedding size
 embed_dim=64
 
 # gpu card id
-dev_id=1
+dev_id=0
 
 # max batch size for training/testing
-batch_size=128
+batch_size=64
 
 net_type=QNet
-decay=0.1
 
 # set reg_hidden=0 to make a linear regression
-reg_hidden=32
+reg_hidden=64
 
 # learning rate
 learning_rate=0.0001
@@ -29,20 +26,20 @@ learning_rate=0.0001
 w_scale=0.01
 
 # nstep
-n_step=1
-
-knn=10
+n_step=2
 
 min_n=15
 max_n=20
+frac_primal=0.2
+edge_prob=0.05
 
-num_env=1
-mem_size=50000
+num_env=10
+mem_size=500000
 
-max_iter=200000
+max_iter=1000000
 
 # folder to save the trained model
-save_dir=$result_root/ntype-$net_type-embed-$embed_dim-nbp-$max_bp_iter-rh-$reg_hidden
+save_dir=$result_root/embed-$embed_dim-nbp-$max_bp_iter-rh-$reg_hidden-primal-$frac_primal-ep-$edge_prob
 
 if [ ! -e $save_dir ];
 then
@@ -50,17 +47,15 @@ then
 fi
 
 python main.py \
-    -net_type $net_type \
+    -dev_id $dev_id \
     -n_step $n_step \
-    -data_root ../../data/tsp2d \
-    -decay $decay \
-    -knn $knn \
     -min_n $min_n \
     -max_n $max_n \
+    -frac_primal $frac_primal \
+    -edge_prob $edge_prob \
     -num_env $num_env \
     -max_iter $max_iter \
     -mem_size $mem_size \
-    -g_type $g_type \
     -learning_rate $learning_rate \
     -max_bp_iter $max_bp_iter \
     -net_type $net_type \
@@ -73,3 +68,4 @@ python main.py \
     -l2 0.00 \
     -w_scale $w_scale \
     2>&1 | tee $save_dir/log-$min_n-${max_n}.txt
+#    -load_model $save_dir/iter_5.model \
