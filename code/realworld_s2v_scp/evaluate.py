14dc0162c49b1c93cb06bfc81079cb9b5f533d4b
kangaroo0123 <daihanjun@gmail.com>
2018-02-13 18:56:55 -0500

    scp memetracker

--- code/realworld_s2v_scp/evaluate.py
+++ code/realworld_s2v_scp/evaluate.py
@@ -8,11 +8,8 @@ import sys
 import time
 from tqdm import tqdm
 
-sys.path.append( '%s/mvc_lib' % os.path.dirname(os.path.realpath(__file__)) )
-from mvc_lib import MvcLib
-
-sys.path.append( '%s/../memetracker' % os.path.dirname(os.path.realpath(__file__)) )
-from meme import *
+sys.path.append( '%s/setcover_lib' % os.path.dirname(os.path.realpath(__file__)) )
+from setcover_lib import SetCoverLib
 
 def find_model_file(opt):
     max_n = int(opt['max_n'])
@@ -32,16 +29,14 @@ def find_model_file(opt):
                     best_it = it
     assert best_it >= 0
     print 'using iter=', best_it, 'with r=', best_r
-    return '%s/iter_%d.model' % (opt['save_dir'], best_it)
-    
+    return '%s/pq-%s_iter_%d.model' % (opt['save_dir'], opt['pq'], best_it)
+
 if __name__ == '__main__':
-    api = MvcLib(sys.argv)
+    api = SetCoverLib(sys.argv)
     
     opt = {}
     for i in range(1, len(sys.argv), 2):
         opt[sys.argv[i][1:]] = sys.argv[i + 1]
-
-    g, _ = build_full_graph('%s/InfoNet5000Q1000NEXP.txt' % opt['data_root'],'undirected')
 
     model_file = find_model_file(opt)
     assert model_file is not None
@@ -49,19 +44,25 @@ if __name__ == '__main__':
     sys.stdout.flush()
     api.LoadModel(model_file)
 
-    result_file = '%s/gnn-%s-%s.csv' % (opt['save_dir'], opt['min_n'], opt['max_n'])
+    frac = 0.0
+
+    test_name = opt['pq']
+    result_file = '%s/test-pq-%s-method-gnn-%s-%s.csv' % (opt['save_dir'], test_name, opt['min_n'], opt['max_n'])
+    f = open(opt['data_test'], 'rb')
 
     with open(result_file, 'w') as f_out:
         print 'testing'
         sys.stdout.flush()
-        api.InsertGraph(g, is_test=True)
-        t1 = time.time()
-        val, sol = api.GetSol(0, nx.number_of_nodes(g))
-        t2 = time.time()
-        f_out.write('%.8f,' % val)
-        f_out.write('%d' % sol[0])
-        for i in range(sol[0]):
-            f_out.write(' %d' % sol[i + 1])
-        f_out.write(',%.6f\n' % (t2 - t1))
-    
-    print 'average size of vc: ', val
+        for i in tqdm(range(1000)):
+            g = cp.load(f)
+            api.InsertGraph(g, is_test=True)
+            t1 = time.time()
+            val, sol = api.GetSol(i, nx.number_of_nodes(g))
+            t2 = time.time()
+            f_out.write('%.8f,' % val)
+            f_out.write('%d' % sol[0])
+            for i in range(sol[0]):
+                f_out.write(' %d' % sol[i + 1])
+            f_out.write(',%.6f\n' % (t2 - t1))
+            frac += val
+    print 'average size of vc: ', frac / 1000.0
--- code/realworld_s2v_scp/main.py
+++ code/realworld_s2v_scp/main.py
@@ -6,55 +6,40 @@ import ctypes
 import os
 import sys
 from tqdm import tqdm
-
-sys.path.append( '%s/mvc_lib' % os.path.dirname(os.path.realpath(__file__)) )
-from mvc_lib import MvcLib
+sys.path.append( '%s/setcover_lib' % os.path.dirname(os.path.realpath(__file__)) )
+from setcover_lib import SetCoverLib
 
 sys.path.append( '%s/../memetracker' % os.path.dirname(os.path.realpath(__file__)) )
 from meme import *
+
+nvalid = 100
 
 def gen_new_graphs(opt):
     print 'generating new training graphs'
     sys.stdout.flush()
     api.ClearTrainGraphs()
     for i in tqdm(range(100)):
-        while True:
-            g = get_mvc_graph(g_undirected,prob_quotient=float(opt['prob_q']))
-            assert len(g)
-            if len(g) > 300:
-                continue
-            break
+        g = get_scp_graph(g_undirected, float(opt['pq']))
         api.InsertGraph(g, is_test=False)
 
-def greedy(G):
-    covered_set = set()
-    numCoveredEdges = 0
-    idxes = range(nx.number_of_nodes(G))
-    idxes = sorted(idxes, key=lambda x: len(nx.neighbors(G, x)), reverse=True)
-    pos = 0
-    while numCoveredEdges < nx.number_of_edges(G):
-        new_action = idxes[pos]
-        covered_set.add(new_action)
-        for neigh in nx.neighbors(G, new_action):
-            if neigh not in covered_set:
-                numCoveredEdges += 1
-        pos += 1
-    print 'done'
-    return len(covered_set)
+def PrepareValidData(opt):
+    print 'generating validation graphs'
+    sys.stdout.flush()
+    f = open(opt['data_test'], 'rb')
+    for i in tqdm(range(nvalid)):
+        g = cp.load(f)
+        api.InsertGraph(g, is_test=True)
 
 if __name__ == '__main__':
-    api = MvcLib(sys.argv)
+    api = SetCoverLib(sys.argv)
     
     opt = {}
     for i in range(1, len(sys.argv), 2):
         opt[sys.argv[i][1:]] = sys.argv[i + 1]
-    
-    g_undirected, _ = build_full_graph('%s/InfoNet5000Q1000NEXP.txt' % opt['data_root'],'undirected')
-    print(nx.number_of_nodes(g_undirected))
-    print(nx.number_of_edges(g_undirected))
-    print greedy(g_undirected)
 
-    api.InsertGraph(g_undirected, is_test=True)
+    g_undirected, _ = build_full_graph('%s/InfoNet5000Q1000NEXP.txt' % opt['data_root'],'undirected')
+
+    PrepareValidData(opt)
 
     # startup
     gen_new_graphs(opt)
@@ -73,13 +58,15 @@ if __name__ == '__main__':
             api.lib.PlayGame(10, ctypes.c_double(eps))
 
         if iter % 300 == 0:
-            frac = api.lib.Test(0)
-            print 'iter', iter, 'eps', eps, 'average pct of vc: ', frac
+            frac = 0.0
+            for idx in range(nvalid):
+                frac += api.lib.Test(idx)
+            print 'iter', iter, 'eps', eps, 'average size of cover: ', frac / nvalid
             sys.stdout.flush()
-            model_path = '%s/iter_%d.model' % (opt['save_dir'], iter)
+            model_path = '%s/pq-%s_iter_%d.model' % (opt['save_dir'], opt['pq'], iter)
             api.SaveModel(model_path)
 
-        if iter % 1000 == 0:
+        if iter % 500 == 0:
             api.TakeSnapshot()
 
         api.lib.Fit()
